06b0de91dad908d7be78af2dfb9c5ecd4987b47c
Overlord7667 <aspiaro@gmail.com>
2023-02-25 20:23:55 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,70 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+	System.out.println("\t\t\t\t       *\n" +
+            "\t\t\t\t      ***\n" +
+            "\t\t\t\t     *****\n" +
+            "\t\t\t\t    *******\n" +
+            "\t\t\t\t   *********\n" +
+            "\t\t\t\t  ***********\n" +
+            "\t\t\t\t *************\n" +
+            "\t\t\t\t***************\n");
+	System.out.println("\t\t\t\t*\n" +
+            "\t\t\t\t* *\n" +
+            "\t\t\t\t* * *\n" +
+            "\t\t\t\t* * * *\n" +
+            "\t\t\t\t* * * * *\n" +
+            "\t\t\t\t* * * * * *\n" +
+            "\t\t\t\t* * * * * * *\n" +
+            "\t\t\t\t* * * * * * * *\n");
+	System.out.println("\t\t\t\t* * * * * * * *\n" +
+            "\t\t\t\t* * * * * * *\n" +
+            "\t\t\t\t* * * * * *\n" +
+            "\t\t\t\t* * * * *\n" +
+            "\t\t\t\t* * * *\n" +
+            "\t\t\t\t* * *\n" +
+            "\t\t\t\t* *\n" +
+            "\t\t\t\t*\n");
+	System.out.println("\t\t\t\t*      \t            *\n" +
+            "\t\t\t\t *       \t       *\n" +
+            "\t\t\t\t  *      \t      *\n" +
+            "\t\t\t\t   *     \t     *\n" +
+            "\t\t\t\t    *    \t    *\n" +
+            "\t\t\t\t     *   \t   *\n" +
+            "\t\t\t\t      *  \t  *\n" +
+            "\t\t\t\t       * \t *\n");
+	System.out.println("\t\t\t\t*\t*\t\t\n" +
+            "\t\t  *\t\t\t\t  *\n" +
+            "\t   *\t\t\t\t     *\n\n" +
+            "\t*\t\t\t\t\t\t\t*\n" +
+            "\t*\t\t\t\t\t\t\t*\n\n" +
+            "\t   *\t\t\t\t     *\n" +
+            "\t\t  *\t\t\t\t  *\n" +
+            "\t\t\t\t*\t*\t\t\n");
+        System.out.println("\t\t\t\t              *\n" +
+                "\t\t\t\t            * * *\n" +
+                "\t\t\t\t          * * * * *\n" +
+                "\t\t\t\t        * * * * * * *\n" +
+                "\t\t\t\t      * * * * * * * * *\n" +
+                "\t\t\t\t    * * * * * * * * * * *\n" +
+                "\t\t\t\t  * * * * * * * * * * * * *\n" +
+                "\t\t\t\t* * * * * * * * * * * * * * *\n" +
+                "\t\t\t\t  * * * * * * * * * * * * *\n" +
+                "\t\t\t\t    * * * * * * * * * * *\n" +
+                "\t\t\t\t      * * * * * * * * *\n" +
+                "\t\t\t\t        * * * * * * *\n" +
+                "\t\t\t\t          * * * * *\n" +
+                "\t\t\t\t            * * *\n" +
+                "\t\t\t\t              *\n");
+        System.out.println("*\t*\t*\t*\t*\n" +
+        "*\t*\t*\t*\t*\n" +
+        "*\t*\t*\t*\t*\n" +
+        "*\t*\t*\t*\t*\n" +
+        "*\t*\t*\t*\t*\n" +
+        "*\t*\t*\t*\t*\n" +
+        "*\t*\t*\t*\t*\n" +
+        "*\t*\t*\t*\t*");
+    }
+
+}
